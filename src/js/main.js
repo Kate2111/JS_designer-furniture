6972ee44283415ebddfd6a034c2838f05c7ed459
Kate2111 <noskova2111@mail.ru>
2023-04-21 16:30:52 +0400

    done header, footer, rightside-menu

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function() {
+    function openRightsideMenu() {
+        const gamburger = document.querySelector('.header__gamburger');
+        const closeButton = document.querySelector('.rightside-menu__close');
+        const rightsideMenu = document.querySelector('.rightside-menu');
+       
+        gamburger.addEventListener('click', function openRightMenu () {
+            
+            rightsideMenu.classList.add('rightside-menu-active');
+        });
+
+        closeButton.addEventListener('click', function closeRightMenu() {
+            rightsideMenu.classList.remove('rightside-menu-active');
+        })
+    }
+
+    openRightsideMenu();
+
+
+})
